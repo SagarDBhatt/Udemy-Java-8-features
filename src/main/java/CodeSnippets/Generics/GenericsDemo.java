b421dfe19aeb5bcdee599d0158edc421372e2cf1
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-06-17 15:57:53 -0600

    Update

--- src/main/java/CodeSnippets/Generics/GenericsDemo.java
+++ src/main/java/CodeSnippets/Generics/GenericsDemo.java
@@ -15,6 +15,11 @@ public class GenericsDemo {
 
         //Here Dev passed "String" type array to call the same method "ShowValue()"
         object.showValues(strArray);
+
+        // Passing object of class Generic object, class User and Class mentor to call the same method.
+        object.showObject(new GenericsDemo());
+        object.showObject(new user());
+        object.showObject(new mentor());
     }
 
     /**
@@ -25,11 +30,26 @@ public class GenericsDemo {
      * @param <T>
      */
     public <T> void showValues( T[] t) {
-
         for(T input : t){
             System.out.println("Values = "+ input);
         }
-
     }
+
+    /**
+     * This Generic method is used to show object value based on the object passed in argument.
+     * @param t
+     * @param <T>
+     */
+    public <T> void showObject (T t){
+        System.out.println("Object is = " + t.toString());
+    }
+
+}//END OF CLASS
+
+class user{
+
+}
+
+class mentor{
 
 }
