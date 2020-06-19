7938fcfb8ff960718f48b92a8fc5fb1528aac78d
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-06-18 22:05:04 -0600

    Update

--- src/main/java/CodeSnippets/Generics/WildCardCharacterInGenerics.java
+++ src/main/java/CodeSnippets/Generics/WildCardCharacterInGenerics.java
@@ -3,14 +3,29 @@ package CodeSnippets.Generics;
 public class WildCardCharacterInGenerics {
 
     public static void main(String[] args) {
+        WildCardCharacterInGenerics object = new WildCardCharacterInGenerics();
+        Machine aMachine = new Machine();
+        Camera aCamera = new Camera();
 
+        object.getObject(aMachine);
+        object.getObject(aCamera);
+
+    }//end of main()
+
+    public <T> void getObject(T t){
+        System.out.println("Object of class -- " + t);
     }
+
 }//End of class
 
 class Machine{
-
+    public String toString(){
+        return "This is Object of class Machine";
+    }
 }
 
-class camera{
-    
+class Camera extends Machine{
+    public String toString(){
+        return "This is object of calss Camera";
+    }
 }
