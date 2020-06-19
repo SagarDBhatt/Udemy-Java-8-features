6f9eebd0088d305738b9b631b588e9d228706bf8
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-06-19 15:56:34 -0600

    File read operation

--- src/main/java/CodeSnippets/FileRead/Reader.java
+++ src/main/java/CodeSnippets/FileRead/Reader.java
@@ -1,6 +1,22 @@
 package CodeSnippets.FileRead;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Reader {
+    public static void main(String[] args) throws FileNotFoundException {
+        String fileName = "C:\\Personal Projects\\Certifications\\Udemy\\Java 8 Concepts\\src\\main\\java\\CodeSnippets\\FileRead\\ReaderFile";
+        File fileObject = new File(fileName);
 
+        //File fileObject = new File("C:\\Personal Projects\\Certifications\\Udemy\\Java 8 Concepts\\src\\main\\java\\CodeSnippets\\FileRead\\ReaderFile");
 
+        Scanner scan = new Scanner(fileObject);
+
+        while(scan.hasNextLine()){
+            String lineRead = scan.nextLine();
+            System.out.println(lineRead);
+        }
+
+    }
 }
