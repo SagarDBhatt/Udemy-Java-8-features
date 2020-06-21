7699a715ff556276c7d42274c94c62071a29af25
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-06-21 13:53:38 -0600

    Update Notes

--- src/main/java/CodeSnippets/Exceptions/ExceptionClass.java
+++ src/main/java/CodeSnippets/Exceptions/ExceptionClass.java
@@ -1,10 +1,20 @@
 package CodeSnippets.Exceptions;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ExceptionClass {
 
-    //Reading a file should check the "File not found" exception.
-    FileReader fr = new FileReader(new File("C://test.txt"));
+    public static void main(String[] args) {
+
+        try{
+            //Reading a file should check the "File not found" exception.
+            FileReader fr = new FileReader(new File("C://test.txt"));
+        }
+
+        catch (FileNotFoundException e){
+            System.out.println("File not found!");
+        }
+    }
 }
