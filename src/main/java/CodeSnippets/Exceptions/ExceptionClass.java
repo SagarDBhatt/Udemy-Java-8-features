0649176013c6198cd3e33611369fcfa025f8e53c
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-06-21 15:12:53 -0600

    Update exceptions in methods

--- src/main/java/CodeSnippets/Exceptions/ExceptionClass.java
+++ src/main/java/CodeSnippets/Exceptions/ExceptionClass.java
@@ -8,13 +8,25 @@ public class ExceptionClass {
 
     public static void main(String[] args) {
 
-        try{
+/*        try{
             //Reading a file should check the "File not found" exception.
             FileReader fr = new FileReader(new File("C://test.txt"));
         }
 
         catch (FileNotFoundException e){
             System.out.println("File not found!");
+        }*/
+        try{
+            readFile();
         }
+        catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+
+    }//End of main()
+
+    public static void readFile() throws FileNotFoundException{
+        File file = new File("text.txt");
+        FileReader fr = new FileReader(file);
     }
-}
+}//End of Class
