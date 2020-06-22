1d3a3c45915a33edc0b203ff5f2172f0b7c77df6
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-06-21 18:20:49 -0600

    Update in Multiple Try/Catch blocks, Multi level exception

--- src/main/java/CodeSnippets/Exceptions/ExceptionClass.java
+++ src/main/java/CodeSnippets/Exceptions/ExceptionClass.java
@@ -3,6 +3,7 @@ package CodeSnippets.Exceptions;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ExceptionClass {
 
@@ -23,10 +24,40 @@ public class ExceptionClass {
             System.out.println("File not found");
         }
 
+        try{
+            runTimeExceptin();
+        }
+        catch (RuntimeException e){
+            System.out.println("Runtime Exception == " + e.toString());
+        }
+
+        try {
+            multipleException();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
     }//End of main()
 
     public static void readFile() throws FileNotFoundException{
         File file = new File("text.txt");
         FileReader fr = new FileReader(file);
+        //throw new FileNotFoundException();
+    }
+
+    public static void runTimeExceptin(){
+        int a = 1;
+        System.out.println(a/0);
+        throw new RuntimeException();
+    }
+
+    public static void multipleException() throws IOException  {
+        File fl = new File("null");
+        FileReader fr = new FileReader(fl);
+        throw new FileNotFoundException();
     }
 }//End of Class
