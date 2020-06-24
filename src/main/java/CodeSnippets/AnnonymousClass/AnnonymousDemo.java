7d621e8bec840fff4ac54584f61841f0e26cafb1
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-06-23 21:06:48 -0600

    Update

--- src/main/java/CodeSnippets/AnnonymousClass/AnnonymousDemo.java
+++ src/main/java/CodeSnippets/AnnonymousClass/AnnonymousDemo.java
@@ -7,7 +7,14 @@ public class AnnonymousDemo {
         AnnonymousDemo obj1 = new AnnonymousDemo();
         obj1.showMessage();
 
-        
+        // Now, create an annonymous class and override method message(). Object of an annonymous class access the overriden "message()" method.
+        Test obj = new Test(){
+            @Override
+            public void message(){
+                System.out.println("Inside inner annonymous class");
+            }
+        };
+        obj.message();
     }
 
     public void showMessage(){
---
+++ src/main/java/CodeSnippets/AnnonymousClass/Test.java
@@ -0,0 +1,8 @@
+package CodeSnippets.AnnonymousClass;
+
+public class Test {
+
+    public void message(){
+        System.out.println("Inside Test class");
+    }
+}
