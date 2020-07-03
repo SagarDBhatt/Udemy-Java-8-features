d2dd4586a4e81168f184dabc83f9f77de18907d1
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-07-03 16:21:42 -0600

    Notes on LinkedHashMap and TreeMap

--- src/main/java/Section2_CollectionFramework/Hashmap/DemoHashMap.java
+++ src/main/java/Section2_CollectionFramework/Hashmap/DemoHashMap.java
@@ -13,11 +13,17 @@ public class DemoHashMap {
 
         System.out.println(map.get(1));
 
+        /**
+         * Print key and value of the HashMap.
+         */
         for(Integer i : map.keySet()){
             //System.out.println("Keys == " + i);
             System.out.println(i + " : " + map.get(i));
         }
 
+        //To remove an element. Provide key to remove that data from HashMap.
+        map.remove(3);
+        System.out.println("Remove Key = 3");
 
     }
 }
