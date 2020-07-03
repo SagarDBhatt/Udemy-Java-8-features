c82fa3283c09a7c8800f1faf7883ec729960cd75
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-07-03 15:04:18 -0600

    HashMap in collection framework.
    Create Demo.java class and lecture notes

---
+++ src/main/java/Section2_CollectionFramework/Hashmap/DemoHashMap.java
@@ -0,0 +1,18 @@
+package Section2_CollectionFramework.Hashmap;
+
+import java.util.HashMap;
+
+public class DemoHashMap {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+
+        map.put(1,"One");
+        map.put(2,"Two");
+        map.put(3,"Three");
+
+        System.out.println(map.get(1));
+
+
+    }
+}
