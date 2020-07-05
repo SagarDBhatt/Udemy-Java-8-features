db9bca085a7cd4b5adb1eb6bd8e2f4c3b2bd72f2
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-07-05 12:36:41 -0600

    Demo on TreeSet and LinkedHashSet
    Include the demo code and Notes

--- src/main/java/Section2_CollectionFramework/Set/demoSet.java
+++ src/main/java/Section2_CollectionFramework/Set/demoSet.java
@@ -1,11 +1,15 @@
 package Section2_CollectionFramework.Set;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class demoSet {
 
     public static void main(String[] args) {
+
+        //HashSet implementation of Set interface:
         Set aSet = new HashSet();   //Set interface provide only one argument<key> and not <key, value> pair.
 
         aSet.add("A");
@@ -13,7 +17,30 @@ public class demoSet {
         aSet.add("C");
         aSet.add("D");
 
-        printSet(aSet,"HashSet");
+//        printSet(aSet,"HashSet");
+
+        //LinkedHash Set :: To maintain an order of insertion of the objects.
+        Set<Integer> linkedSet = new LinkedHashSet<Integer>();
+
+        linkedSet.add(8);
+        linkedSet.add(20);
+        linkedSet.add(4);
+        linkedSet.add(1);
+
+//        printSet(linkedSet,"LinkedHashSet");
+
+        //TreeSet : To sort the objects based on their natural order i,e, 1,2,3,4... and a,b,c,d,e...
+
+        Set<Integer> treeSet = new TreeSet<Integer>();
+
+        treeSet.add(10);
+        treeSet.add(9);
+        treeSet.add(24);
+        treeSet.add(16);
+
+        printSet(treeSet,"TreeSet");
+
+
     }
 
     public static void printSet(Set set, String type){
