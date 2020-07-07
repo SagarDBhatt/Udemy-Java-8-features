cbefb8c91aae446dc923406feb78988c32a2fab9
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-07-06 21:22:39 -0600

    Demo classes for Comparator interface
    Class with a few properties with ArrayList contains objects of the class.
    
    Use comparator interface to define the sorting parameter.

---
+++ src/main/java/Section2_CollectionFramework/ComparatorInterface/RunnerClass.java
@@ -0,0 +1,22 @@
+package Section2_CollectionFramework.ComparatorInterface;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class RunnerClass {
+
+    public static void main(String[] args) {
+        Person p1 = new Person(1,25,"Sam");
+
+        ArrayList<Person> listPerson = new ArrayList<Person>();
+        listPerson.add(p1);
+        listPerson.add(new Person(2,28,"Rob"));
+        listPerson.add(new Person(3,30,"Dan"));
+        listPerson.add(new Person(4,29,"Lynn"));
+
+        Collections.sort(listPerson);
+
+
+    }
+}
