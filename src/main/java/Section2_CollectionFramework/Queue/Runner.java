841c7740186d259666d5e515a6261f90c98fa6b2
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-07-16 21:05:14 -0600

    Update demo class and notes

--- src/main/java/Section2_CollectionFramework/Queue/Runner.java
+++ src/main/java/Section2_CollectionFramework/Queue/Runner.java
@@ -1,7 +1,20 @@
 package Section2_CollectionFramework.Queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Runner {
 
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<Integer>();
 
+        for(int i=1;i<=8;i++)
+        q.add(i);
 
+        for(Integer val : q){
+            System.out.println("Queue value = " + val);
+        }
+
+        System.out.println("Elements of Queue = " + q);
+    }
 }
