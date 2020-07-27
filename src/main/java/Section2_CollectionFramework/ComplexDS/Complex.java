98525b928715cd0a7ba62df808ca1b2708309a67
Sagar Bhatt <49220046+SagarDBhatt@users.noreply.github.com>
2020-07-26 23:44:32 -0600

    Create Comple.java
    To implement the solution of the problwm

---
+++ src/main/java/Section2_CollectionFramework/ComplexDS/Complex.java
@@ -0,0 +1,14 @@
+package Section2_CollectionFramework.ComplexDS;
+
+public class Complex {
+
+    public static String[] vehicles = { "Bus","Car","Van","Bike" };
+
+    public static String[][] drivers = {
+            {"Ram","Sam","Kan"},
+            {"Josh","Kosh","Hosh","Rosh"},
+            {"Ja","Ra","Ma"},
+            {"Ken","Ren"}
+    };
+
+}
