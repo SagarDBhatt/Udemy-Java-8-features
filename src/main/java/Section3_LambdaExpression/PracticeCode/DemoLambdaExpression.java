a32ae6ba9de6559e5b9b6e0e6b097e4b0253af60
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-07-30 11:52:34 -0600

    create single args, multi args, return type and multi line functional interace.
    Create various functional interfaces & implement in demo class.

--- src/main/java/Section3_LambdaExpression/PracticeCode/DemoLambdaExpression.java
+++ src/main/java/Section3_LambdaExpression/PracticeCode/DemoLambdaExpression.java
@@ -18,6 +18,21 @@ public class DemoLambdaExpression {
         };
         interfceObject2.draw();     //calling draw()
 
+        //=============================================================================
 
+        oneArgumentFunctInterface argObject = (x) -> {
+            System.out.println("Value to print == " + x);
+        };
+        argObject.printNumber(10);
+
+        //--------------
+        //--------------
+
+        multiArgsFuncInterface mulObject = (a,b) -> {
+            System.out.println("This is multi-line, multi-args and return type lambda expression");
+            return a*b;
+        };
+
+        System.out.println(mulObject.multiply(12,5));
     }
 }
---
+++ src/main/java/Section3_LambdaExpression/PracticeCode/oneArgumentFunctInterface.java
@@ -0,0 +1,6 @@
+package Section3_LambdaExpression.PracticeCode;
+
+@FunctionalInterface
+public interface oneArgumentFunctInterface {
+    void printNumber(int a);
+}
