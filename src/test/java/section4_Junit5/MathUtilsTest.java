21e52b913604e85e1d4e231094f8901f7814e188
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-08-19 13:55:10 -0600

    Software Test Life cycle annotations
    @BeforeEach, @BeforeAll, @AfterEach, @AfterAll.

--- src/test/java/section4_Junit5/MathUtilsTest.java
+++ src/test/java/section4_Junit5/MathUtilsTest.java
@@ -1,26 +1,30 @@
 package section4_Junit5;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
 
+    MathUtils utils;
+
+    @BeforeEach
+    void init(){
+        utils = new MathUtils();
+    }
+
     @Test
     void add() {
-       MathUtils mathUtils = new MathUtils();
-       int actual = mathUtils.add(1,1);
+       //MathUtils mathUtils = new MathUtils();
+       int actual = utils.add(1,1);
        int expected = 2;
-
-      /* if (actual == expected)
-           System.out.println("This test passed!!");*/
       assertEquals(expected,actual,"The add method should add two numbers");
     }
 
     @Test
     void testCalcCircleArea(){
-        MathUtils utils = new MathUtils();
-
+        //MathUtils utils = new MathUtils();
         double expectedArea = 314.0;
         double actual = utils.calcCircleArea(10);
         assertEquals(expectedArea,actual,"The area of the circle should match");
@@ -28,7 +32,7 @@ class MathUtilsTest {
 
     @Test
     void testDivide(){
-        MathUtils utils = new MathUtils();
+        //MathUtils utils = new MathUtils();
         assertThrows(ArithmeticException.class, () -> utils.divide(1,0),"Divide by zero");
     }
 }
